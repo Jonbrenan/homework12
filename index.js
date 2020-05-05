5a288d6ea4748ccedbbd5517daac0f80b4577af1
Jon Brenan <jonathanbrenan@gmail.com>
2020-05-05 13:35:47 -0400

    project complete for most part, just need to make a few tweaks and I want a version of this that has everything working

--- index.js
+++ index.js
@@ -3,35 +3,6 @@ let inquirer = require('inquirer')
 const mysql = require('mysql2/promise')
 
 
-// const actionquestions = [
-//   {
-//     type: 'list',
-//     name: 'action',
-//     message: 'what do you want to do?',
-//     choices: [
-//       {
-//           name: 'Add department',
-//           value: 'ADD_DEPARTMENT'
-//       },
-//       {
-//         name: 'Add employee',
-//         value: 'ADD_EMPLOYEE'
-//       }
-//     ]
-//   }
-// ]
-// const answers = await prompt(questions)
-
-// switch(answers.action){
-//   case "ADD_DEPARTMENT":
-//     // RUN INQUIRER ASK FOR THE REQUIRED VALUES
-//     // SQL INSERT
-//     // Ask action questions again
-//   case "ADD_EMPLOYEE":
-//     //
-  
-// }
-
 
 
 inquirer
